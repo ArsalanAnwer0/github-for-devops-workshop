9754b669e140c0cb63b39e1064e4e5799fee6040
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2024-12-29 19:45:18 -0600

    testing python file

--- testing.py
+++ testing.py
@@ -1,1 +1,15 @@
 print("Hello friends, I am testing.py")
+
+# Function to add two numbers
+def add_two_numbers(num1, num2):
+    return num1 + num2
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating the sum
+sum = add_two_numbers(num1, num2)
+
+# Displaying the sum
+print(f"The sum is: {sum}")
